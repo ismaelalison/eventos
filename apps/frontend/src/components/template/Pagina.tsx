a90788ff7850525c8d1bdcfc9397c667e74b4135
ismaelalison <ismael_alison4@hotmail.com>
2024-11-21 23:08:10 -0300

    Implementing page component

--- apps/frontend/src/components/template/Pagina.tsx
+++ apps/frontend/src/components/template/Pagina.tsx
@@ -1,3 +1,5 @@
+import Logo from "./Logo";
+
 export interface PaginaProps{
     children: React.ReactNode;
     className?: string;
@@ -5,8 +7,15 @@ export interface PaginaProps{
 
 export default function Pagina(props: PaginaProps) {
     return (
-        <div className="">
-            <main className={props.className}>
+        <div className="
+            flex flex-col items-center py-10 bg-black min-h-screen
+            bg-[url('/background.png')] bg-cover
+        ">
+            <Logo />
+            <main className={`
+                    flex-1 flex flex-col justify-center py-10 
+                    container ${props.className}
+                `}>
                 {props.children}
             </main>
         </div>
