e7f79ec29fecefa60823b343d06c3a6f010e65ee
ismaelalison <ismael_alison4@hotmail.com>
2024-11-21 22:44:59 -0300

    Starting the project

---
+++ apps/frontend/src/components/template/Pagina.tsx
@@ -0,0 +1,14 @@
+export interface PaginaProps{
+    children: React.ReactNode;
+    className?: string;
+}
+
+export default function Pagina(props: PaginaProps) {
+    return (
+        <div className="">
+            <main className={props.className}>
+                {props.children}
+            </main>
+        </div>
+    )
+}
